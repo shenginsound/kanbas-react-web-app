d9e473e8827f02af5afa52edbb2d2bea25a6904d
鄭家昇 <zhengjiasheng@zhengjiashengdeMacBook-Pro.local>
2023-12-01 15:47:10 -0500

    a6 Mongo fa23

--- src/project/index.js
+++ src/project/index.js
@@ -7,10 +7,14 @@ import Details from "./details";
 import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import SportsMap from "./map";
+import Signin from "./users/signin";
+import Account from "./users/account";
 // import UserList from "./users/list";
 // import UserDetails from "./users/details";
 // import SignIn from "./users/signin";
 // import Account from "./users/account";
+import UserTable from "./users/table";
+import Signup from "./users/signup";
 
 
 function Project(){
@@ -35,6 +39,17 @@ function Project(){
             <Link to="/project/search" className="list-group-item">
               Search
             </Link>
+            <Link to="/project/signin" className="list-group-item">
+              Signin
+            </Link>
+            <Link to="/project/signup" className="list-group-item">
+              SignUp
+            </Link>
+            <Link to="/project/admin/users" className="list-group-item">
+              UserTable</Link>
+            <Link to="/project/account" className="list-group-item">
+              Account
+            </Link>
             <Link to="/project/map" className="list-group-item">
               SportsMap
             </Link>
@@ -48,6 +63,13 @@ function Project(){
         <div className="col-10">
         <Routes>
             <Route path="/" element ={<Home/>} ></Route>;
+            <Route path="/signin" element={<Signin />} ></Route>;
+            <Route path="/signup" element={<Signup />} ></Route>;
+            
+            <Route path="/admin/users" element={<UserTable />} />
+
+            <Route path="/account" element={<Account />} ></Route>;
+            <Route path="/account/:id" element={<Account />} />
             <Route path="/profile" element ={<Profile/>} ></Route>;
             <Route path="/search" element ={<Search/>} ></Route>;
             <Route path="/details" element ={<Details/>} ></Route>;
